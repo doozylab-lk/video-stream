20639cb0101fbb28dffbfcf4568895faf35e7918
DOOZY YEZ <94822722+doozylab-lk@users.noreply.github.com>
2021-11-23 12:01:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 from pytgcalls import idle
-from driver.veez import call_py, bot
+from driver.doozy import call_py, bot
 
 async def mulai_bot():
     print("[INFO]: STARTING BOT CLIENT")
