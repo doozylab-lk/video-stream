2c7df8376696120a5e31482f2b093895a1ebda1f
ᴅᴏᴏᴢʏ <94822722+doozylab-lk@users.noreply.github.com>
2021-11-26 23:56:53 +0530

    Update video.py

--- program/video.py
+++ program/video.py
@@ -4,7 +4,7 @@ import re
 from config import ASSISTANT_NAME, BOT_USERNAME, IMG_1, IMG_2, UPDATES_CHANNEL
 from driver.filters import command, other_filters
 from driver.queues import QUEUE, add_to_queue
-from driver.veez import call_py, user
+from driver.doozy import call_py, user
 from pyrogram import Client
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
