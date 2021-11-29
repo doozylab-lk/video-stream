2ca495b34ced872a14f256cbe9cd357b7dfd2bb3
ᴅᴏᴏᴢʏ <94822722+doozylab-lk@users.noreply.github.com>
2021-11-29 09:37:53 +0530

    Update start.py

--- program/start.py
+++ program/start.py
@@ -55,9 +55,9 @@ async def start_(client: Client, message: Message):
         f"""✨ **Welcome {message.from_user.mention()} !**\n
 💭 [{BOT_NAME}](https://t.me/{BOT_USERNAME}) **Allows you to play music and video on groups through the new Telegram's video chats!**
 
-💡 **Find out all the Bot's commands and how they work by clicking on the » 📚 Commands button!**
+💡 **Find out all the Bot's commands and how they work by clicking on the »  Commands button!**
 
-🔖 **To know how to use this bot, please click on the » ❓ Basic Guide button!**
+🔖 **To know how to use this bot, please click on the » Basic Guide button!**
 """,
         reply_markup=InlineKeyboardMarkup(
             [
@@ -89,6 +89,8 @@ async def start_(client: Client, message: Message):
         ),
         disable_web_page_preview=True,
     )
+
+DOOZYLABLK_STICKER = "https://t.me/addstickers/gogoli_pack_4"
 
 
 @Client.on_message(
