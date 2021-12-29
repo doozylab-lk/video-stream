d6167b737a47c9fa109e58d981718a0e4f57f81a
❰❰ꪶ⁩DOOZY❱ <94822722+doozylab-lk@users.noreply.github.com>
2021-12-29 21:57:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import asyncio
 from pytgcalls import idle
 from driver.doozy import call_py, bot
 
-async def mulai_bot():
+async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
     await bot.start()
     print("[INFO]: STARTING PYTGCALLS CLIENT")
     await call_py.start()
     await idle()
-    print("[INFO]: STOPPING BOT")
+    print("[INFO]: STOPPING BOT & USERBOT")
     await bot.stop()
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(mulai_bot())
+loop.run_until_complete(start_bot())
